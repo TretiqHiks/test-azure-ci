f18290d8f33203528512991978dde9a7889632a6
Svetoslav <i451396@FontysICT.nl>
2024-06-22 22:36:59 +0200

    add dockerfile

--- src/main/java/com/example/ci_demo/TestController.java
+++ src/main/java/com/example/ci_demo/TestController.java
@@ -1,6 +1,7 @@
 package com.example.ci_demo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    public ResponseEntity<>
+    @GetMapping("/endpoint")
+    public String testEndpoint(){
+        return "Test";
+    }
 }
--- src/test/java/com/example/ci_demo/CiDemoApplicationTests.java
+++ src/test/java/com/example/ci_demo/CiDemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.ci_demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CiDemoApplicationTests {
 
+    @Autowired
+    TestService testService;
+
     @Test
     void contextLoads() {
+
     }
+
+    @Test
+    public void testAddition() {
+        assertEquals(5, testService.addUp(2, 3));
+    }
+
+
 
 }
